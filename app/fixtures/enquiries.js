097ac319a56913ffcfe6c3fe232acbb2466970db
Robert Preus-MacLaren <rpprroger@googlemail.com>
2013-12-12 17:19:44 +0000

    Use generated deploy section

--- app/fixtures/enquiries.js
+++ app/fixtures/enquiries.js
@@ -2,9 +2,7 @@ define(['can/util/string', 'can/util/fixture'], function(can){
     'use strict';
 
     can.fixture({
-        'POST tabs_property/{propRef}/booking/enquiry' : function() {
-            console.log.apply(console, arguments);
-        }
+        'POST tabs_property/{propRef}/booking/enquiry' : 'http://localhost/NeonTABS/demosite/property/THEBEA_HR/booking/enquiry'
     });
 
     return can.fixture;
