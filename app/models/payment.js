40293e95858da8cc2792d13b9611b6e6d8d73f15
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-01-21 15:49:21 +0000

    Added fixtures and files for payments

--- app/models/payment.js
+++ app/models/payment.js
@@ -1,14 +1,24 @@
 /**
  */
-define(['can/util/string', 'can/model'], function( can ){
+define([
+    'can/util/string',
+    'resources/book',
+    'can/model',
+    'can/compute'
+], function( can, booking ){
     'use strict';
 
     return can.Model({
 
+        create: 'POST property/booking/{booking.bookingId}/pay',
+        update: 'POST property/booking/{booking.bookingId}/pay',
+
         defaults: {
-            'bookingId': '',
-            'iframeURL': '',
-            'payMethod': ''
+            'booking': booking,
+            'payLater': false,
+            // balance: pay the full amount
+            // deposit: just pay the deposit
+            'paymentType': 'balance'
         }
     }, {
 
---
+++ app/top.js
@@ -0,0 +1,5 @@
+(function( $, window ) {
+	'use strict';
+
+	$('h1').append('yay jquery');
+})( jQuery, window );
