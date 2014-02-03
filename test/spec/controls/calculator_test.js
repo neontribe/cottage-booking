3aee358d0fd0373dccd0fd1ceb18463bc3f0c3fc
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-02-03 11:00:27 +0000

    Added calculator test file

---
+++ test/spec/controls/calculator_test.js
@@ -0,0 +1,14 @@
+/* jshint unused: false */
+/* global describe, it, expect */
+define([
+    'test',
+    'underscore',
+    'controls/calculator/calculator',
+    'fixtures/enquiries'
+], function( chai, _, Calculator ) {
+    'use strict';
+
+    var should = chai.should();
+
+
+});
