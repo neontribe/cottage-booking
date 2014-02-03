8cfc8a94fea50472d324904d0130a32ca76d2ad7
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-02-03 11:03:38 +0000

    Tidy

--- app/resources/book.js
+++ app/resources/book.js
@@ -2,11 +2,8 @@
  *
  * @return {can.compute} The can.compute wrapped object
  */
-define(['can/util/string', 'models/booking', 'can/observe'], function( can, Booking ) {
+define(['can/util/string', 'models/booking'], function( can, Booking ) {
     'use strict';
-
-    var booking = new Booking();
-
     // Return the model so we can listen for changes
-    return booking;
+    return new Booking();
 });
