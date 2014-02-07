9934bd8118c7e65c81a4211be576a26eec2e29b2
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-02-07 17:18:12 +0000

    Fixes for #11

--- app/fixtures/bookings.js
+++ app/fixtures/bookings.js
@@ -1,12 +1,40 @@
-define(['fixtures/fixtures', 'underscore'], function(can, _){
+define(['fixtures/fixtures', 'underscore'], function(can){
     'use strict';
 
     can.wrapFixture('POST property/booking/create', 'fixtures/bookings/', null, 'property-booking-d76c8badd8448cc9d1d888778966d140.json');
 
     can.wrapFixture('GET property/booking/{bookingId}', 'fixtures/bookings/');
 
+    function change() {
+
+        var target = arguments[0] || {},
+            args = [].slice.call(arguments, 1),
+            looper = function( value, key ) {
+                if( typeof args[i][key] === 'object' ) {
+                    change( target[key], args[i][key] );
+                } else if(args[i][key] !== undefined) {
+                    target[key] = args[i][key];
+                }
+            };
+
+        for (var i = 0; i < args.length; i++) {
+            can.each( target, looper );
+        }
+
+        return target;
+
+    }
+
     can.wrapFixture('POST property/booking/{bookingId}', 'fixtures/bookings/', function( data, status, def, originalAjax ) {
-        return can.useFixtures ? can.extend( data, _.pick( originalAjax.data, _.keys( data ) ) ) : data;
+        var newObject = data;
+
+        if( can.useFixtures ) {
+            // Everything in the fixture is what could be returned
+            // So things are thrown away if they aren't there
+            change( newObject, originalAjax.data );
+        }
+
+        return newObject;
     });
 
     return can;
