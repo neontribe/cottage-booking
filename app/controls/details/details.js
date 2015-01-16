ae8dc52ce2885c8de6c88838738dae9bc7248d73
Toby Batch <tobias@neontribe.co.uk>
2015-01-16 15:49:35 +0000

    Switched autopay to not auto display

--- app/controls/details/details.js
+++ app/controls/details/details.js
@@ -59,7 +59,7 @@ define([
               'default': 'paylater'
             },
             autoPayment: {
-              show: true,
+              show: false,
               label: 'Would you like us to automatically take your balance, when it\'s due',
               labels: {
                 autotakebalance: 'Oui',
