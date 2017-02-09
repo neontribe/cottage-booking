5316795f0533f1d5801fbff5cd33d97693f67338
Oliver Barnwell <barnwelloliver@gmail.com>
2017-02-09 13:13:09 +0000

    Check

--- app/html5shivoverwrite.js
+++ app/html5shivoverwrite.js
@@ -1,6 +1,6 @@
 define([], function() {
     // Enable the up-to-date shiv if it's already been added.
-   if(window.html5 !== undefined) {
+   if(window.html5) {
        require(['html5shiv']);
    }
 });
